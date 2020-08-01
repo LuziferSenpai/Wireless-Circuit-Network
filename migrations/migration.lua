843f40ce9faad229a4d1613fbe8d272306e2e33b
Luzifer <mrcookiesmedia@gmail.com>
2020-08-01 17:22:24 +0200

    Update 0.4.4

--- migrations/migration.lua
+++ migrations/migration.lua
@@ -2,6 +2,6 @@ for index, force in pairs( game.forces ) do
 	local technologies = force.technologies
 	local recipes = force.recipes
 	
-	recipes["Wireless-Sender"].enabled = technologies["circuit-network"].researched
-	recipes["Wireless-Reciever"].enabled = technologies["circuit-network"].researched
+	recipes["wireless-transmitter"].enabled = technologies["circuit-network"].researched
+	recipes["wireless-reciever"].enabled = technologies["circuit-network"].researched
 end
